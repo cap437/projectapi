124dd9fb429362d2c2b511c708f552eec52a70a6
cap437 <Capitalretreval@gmail.com>
2024-07-24 19:21:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ dotenv.config();
 app.use(cookieParser());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
 app.use("/api/users", userRoutes); 
 app.use((err, req, res, next)=>{
     const status = err.status || 500;
@@ -24,15 +32,8 @@ app.use((err, req, res, next)=>{
 
     }) 
 })
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+
 
 app.listen(8800, ()=>{  
-
     console.log('app server running on port 8800....')
 })
